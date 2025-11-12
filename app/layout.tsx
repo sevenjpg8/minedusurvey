7685eeaa369de96d121140c78e0e53d04f691ec0
Valentin <mikeallca27@gmail.com>
2025-11-12 10:36:24 -0500

    se volvio a quitar analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
 import Footer from "@/components/footer"
 import "./globals.css"
 
@@ -24,7 +23,6 @@ export default function RootLayout({
       <body className={`font-sans antialiased flex flex-col min-h-screen`}>
         <div className="flex-1">{children}</div>
         <Footer />
-        <Analytics />
       </body>
     </html>
   )
